99c522ac87af9d0cbcf8e5dbcc5a20cea445b6f4
Devaud <devaud.alan@gmail.com>
2015-02-05 20:34:26 +0100

    Modification css et index

--- MovieHouse/index.php
+++ MovieHouse/index.php
@@ -13,21 +13,28 @@ and open the template in the editor.
     <body>
         <section id="page">
             <section class="sec-sup">
-                
+                <nav class="nav-sup">
+                    <ul>
+                        <li>
+                            <a href="#">Home</a>
+                        </li>
+                        <li>
+                            <a href="#">Home</a>
+                        </li>
+                    </ul>
+                </nav>
+
             </section>
-            <section class="sec-left">
-                
-            </section>
-            
+
             <section class="contenu">
-                
+
                 <section class="box">
                     <section class="box-header">
-                        
+                        <h2>Nouveauter film</h2>
                     </section>
-                    
+
                     <section class="box-body">
-                        
+
                         <article class="artic">
                             <section class="artic-affiche">
                                 <img src="./ressources/bilbo-the-hobbit-affiche-film-1.jpg" alt="the hobbit" class="picture"/>
@@ -40,10 +47,39 @@ and open the template in the editor.
                                 <a href="#">Lire plus</a>
                             </section>
                         </article>
-                        
+
+                        <article class="artic">
+                            <section class="artic-affiche">
+                                <img src="./ressources/bilbo-the-hobbit-affiche-film-1.jpg" alt="the hobbit" class="picture"/>
+                            </section>
+                            <section class="artic-contenu">
+                                <ul>
+                                    <li>Titre : The Hobbit</li>
+                                    <li>Synopsis : Dans UN VOYAGE INATTENDU, Bilbon Sacquet cherche à reprendre le Royaume perdu des Nains d'Erebor, conquis par le redoutable dragon Smaug. ...</li>
+                                </ul>
+                                <a href="#">Lire plus</a>
+                            </section>
+                        </article>
+
+                        <article class="artic">
+                            <section class="artic-affiche">
+                                <img src="./ressources/bilbo-the-hobbit-affiche-film-1.jpg" alt="the hobbit" class="picture"/>
+                            </section>
+                            <section class="artic-contenu">
+                                <ul>
+                                    <li>Titre : The Hobbit</li>
+                                    <li>Synopsis : Dans UN VOYAGE INATTENDU, Bilbon Sacquet cherche à reprendre le Royaume perdu des Nains d'Erebor, conquis par le redoutable dragon Smaug. ...</li>
+                                </ul>
+                                <a href="#">Lire plus</a>
+                            </section>
+                        </article>
+
                     </section>
                 </section>
-                
+
+
+
+
             </section>
         </section>
     </body>
